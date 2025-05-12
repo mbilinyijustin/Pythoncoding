2fe9037a1bf62c574a30678a2f99a6cfa0f77e2d
Justine <mbilinyijustin14@gmail.com>
2025-05-12 12:08:02 +0300

    adding save_user fun

--- todo_app.py
+++ todo_app.py
@@ -5,11 +5,18 @@ import shutil
 TASKS_FILE = "tasks.json"
 USERS_FILE = "users.json"
 
+
 def load_users():
     if os.path.exists(USERS_FILE):
         with open(USERS_FILE, "r") as f:
             return json.load(f)
     return {}
+
+
+def save_users(users):
+    with open(USERS_FILE, "w") as f:
+        json.dump(users, f)
+
 
 def load_tasks():
     try:
