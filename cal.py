73e6c6455dd0c654d68f6d2f6a7b2e2939f96e82
Justine <mbilinyijustin14@gmail.com>
2025-05-18 14:12:16 +0300

    creating if ststment for entering the choice

--- cal.py
+++ cal.py
@@ -33,3 +33,13 @@ while True:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input. Please enter a number between 1 and 5")
