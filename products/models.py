0d5038f2f914a48cfef6f98388ccacc2339ac616
Justine <mbilinyijustin14@gmail.com>
2025-05-22 19:00:38 +0300

    creating product model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 
